6682fcb8d361c03102cf3e58e1c706d8e6b6ccc5
Tobias Quathamer <toddy@debian.org>
2010-09-07 19:08:15 +0200

    Add script to update original manpages from .deb files

---
+++ english/update-manpages.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+# Usage: Put the latest .deb files of the packages into their
+# corresponding directory and run this script.
+
+packages=`find -maxdepth 1 -type d | grep -v "^\.$" | cut -d/ -f2`
+
+for package in $packages; do
+	latest_deb=`ls $package/$package*.deb 2>/dev/null | tail -n1`
+	if [ -z $latest_deb ]; then
+		echo "Warning: Could not find .deb for package '$package'"
+	else
+		ar x $latest_deb data.tar.gz
+		mkdir tmp
+		tar xzf data.tar.gz --directory=tmp/
+		rm -rf $package/man?
+		for mandir in tmp/usr/share/man/man?/; do
+			section=`echo $mandir | cut -d/ -f5`
+			# Only copy directories with files
+			files=`ls $mandir`
+			if [ -n "$files" ]; then
+				mkdir $package/$section
+				# Remove manpages which are links
+				for manpage in $mandir/*; do
+					existing=`readlink $manpage`
+					if [ -n "$existing" ]; then
+						linked_section=`basename $existing .gz | sed -e "s/.\+\.//"`
+						echo man$linked_section/`basename $existing` $section/`basename $manpage` >> tmp.links
+						rm $manpage
+					fi
+				done
+				cp $mandir/* $package/$section
+				gzip -d $package/$section/*
+				# Remove manpages which contain only .so links
+				for manpage in $package/$section/*; do
+					existing=`grep "^\.so" $manpage | sed -e "s/^\.so //"`
+					if [ -n "$existing" ]; then
+						echo $existing.gz $section/`basename $manpage`.gz >> tmp.links
+						rm $manpage
+					fi
+				done
+			fi
+		done
+		LC_ALL=C sort tmp.links > $package/$package.links
+		rm -rf tmp/ data.tar.gz tmp.links
+	fi
+done
