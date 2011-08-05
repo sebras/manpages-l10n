00f845f5895171848720a62ab3ee4d6df7518982
Tobias Quathamer <toddy@debian.org>
2011-08-05 14:33:10 +0200

    Remove translations of manpages which do no longer exist

--- po/update-translations.sh
+++ po/update-translations.sh
@@ -6,19 +6,24 @@ for translation in $translations; do
 	original=`find ../english/ -name $manpage`
 	# Print the progress
 	echo -n "$translation "
-	# Create a custom compendium
-	./generate-custom-compendium.sh $original
-	# Update .po file from master file
-	po4a-updatepo -f man --option groff_code=verbatim --option generated \
-		--option untranslated="a.RE,\|" \
-		-m $original -M ISO-8859-1 \
-		--msgmerge-opt "--backup=none --no-location --compendium custom.po --previous" \
-		--po $translation
-	# Remove obsolete strings
-	msgattrib --no-obsolete $translation > tmp.po
-	# Prefer the translations from the compendium
-	msgmerge --compendium custom.po --no-fuzzy-matching /dev/null tmp.po > result.po
-	mv result.po $translation
+	# If the manpage does no longer exist, remove the translation
+	if [ -z "$original" ]; then
+		git rm "$translation"
+	else
+		# Create a custom compendium
+		./generate-custom-compendium.sh $original
+		# Update .po file from master file
+		po4a-updatepo -f man --option groff_code=verbatim --option generated \
+			--option untranslated="a.RE,\|" \
+			-m $original -M ISO-8859-1 \
+			--msgmerge-opt "--backup=none --no-location --compendium custom.po --previous" \
+			--po $translation
+		# Remove obsolete strings
+		msgattrib --no-obsolete $translation > tmp.po
+		# Prefer the translations from the compendium
+		msgmerge --compendium custom.po --no-fuzzy-matching /dev/null tmp.po > result.po
+		mv result.po $translation
+	fi
 done
 # Cleanup
 rm tmp.po custom.po
