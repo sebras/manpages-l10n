b0a81394ff2ac82f312cf5f9b7558c75ea61b228
Tobias Quathamer <toddy@debian.org>
2011-08-05 14:51:36 +0200

    Do no longer add the addendum

--- work/commit-po.sh
+++ work/commit-po.sh
@@ -11,12 +11,10 @@ program=`basename $1 | sed -e "s/\.[0-9]//"`
 section=`basename $1 | sed -e "s/.\+\.//"`
 
 mv $1.po ../po/man$section/
-mv $1.add ../po/man$section/
 
 # Remove source files.
 rm -f $1 $program.$section.de.$section
 
 git pull
 git add ../po/man$section/$1.po
-git add ../po/man$section/$1.add
 git commit -m "Initial conversion of $1"
