293ddc8c985f020e6df5ddc6b026d4a10f06929d
Tobias Quathamer <toddy@debian.org>
2012-03-06 10:23:27 +0100

    Update .po file creation script for new location

--- po/create-po.sh
+++ po/create-po.sh
@@ -1,32 +1,49 @@
 #!/bin/sh
 
-if [ -z $1 ]; then \
-  echo "Geben Sie die Handbuchseite an." ; \
-  exit ; \
+# Require one argument
+if [ -z "$1" ]; then
+	echo "Specify the manpage."
+	exit 1
 fi
 
-program=`basename $1 | sed -e "s/\.[0-9]//"`
-section=`basename $1 | sed -e "s/.\+\.//"`
-
-translation=`find -name $program.$section.de.$section`
-if [ -z $translation ]; then \
-  echo "Es gibt leider keine deutsche Übersetzung." ; \
-else \
-  translation="-l $translation -L UTF-8" ; \
+# If only the name without path has been provided,
+# try to find the original manpage
+if [ ! -f "$1" ]; then
+	original=`find ../english/ -type f -name "$1"`
+	if [ -z "$original" ]; then
+		echo "The file $1 could not be found."
+		exit 1
+	fi
+else
+	original="$1"
 fi
 
-# Generate with po4a
+name=`basename "$original" | sed -e "s/\.[0-9]//"`
+section=`basename "$original" | sed -e "s/.\+\.//"`
+output=`mktemp`
+
+# Create .po file with po4a
 po4a-gettextize -f man \
  --option groff_code=verbatim \
  --option generated \
  --option untranslated="a.RE,\|" \
  --option unknown_macros=untranslated \
- -m $1 $translation -p $program.$section.po
+ --master "$original" \
+ --po "$output"
 
 # Stop here if po4a fails
-if [ $? -ne 0 ]; then \
-  exit 1; \
+if [ $? -ne 0 ]; then
+	echo "Fehler bei der Ausführung von po4a."
+	rm "$output"
+	exit 1
 fi
 
 # Remove location information
-sed -i -e "/^#: /d" $program.$section.po
+sed -i -e "/^#: /d" "$output"
+
+# Specify encoding
+sed -i -e "s/^\"Content-Type: text\/plain; charset=CHARSET\\\\n\"$/\"Content-Type: text\/plain; charset=UTF-8\\\\n\"/" "$output"
+
+mv "$output" "man$section/$name.$section.po"
+./update-po.sh "man$section/$name.$section.po" CREATE_HEADER
+echo "Created man$section/$name.$section.po"
