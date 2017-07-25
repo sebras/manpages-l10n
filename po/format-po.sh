2eeff10ae80531ea518a7fa422000d79c5e89a60
Dr. Tobias Quathamer <toddy@debian.org>
2017-07-25 16:56:38 +0200

    Rewrite formatting script

--- po/format-po.sh
+++ po/format-po.sh
@@ -1,22 +1,48 @@
 #!/bin/sh
+#
+# Copyright © 2010-2017 Dr. Tobias Quathamer <toddy@debian.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Get the head of the file until first msgid line
-# and filter out all comment lines without year information
-for i in `find man? -name "*.po" | sort`; do
-	sed -n "1,/^msgid/p" "$i" |
-	grep -v "^# German translation of manpages" |
-	grep -v "^# This file is distributed under the same license as the manpages-de package." |
-	grep -v "^# Copyright © of this file:" |
-	grep -v "^#\s*$" > tmp-header.po
-	sed -e "1,/^msgid/d" "$i" > tmp-tail.po
-	echo "# German translation of manpages" > tmp-result.po
-	echo "# This file is distributed under the same license as the manpages-de package." >> tmp-result.po
-	echo "# Copyright © of this file:" >> tmp-result.po
-	cat tmp-result.po tmp-header.po tmp-tail.po > "$i"
-	# Now remove location information, which is not needed in our case
-	sed -i -e "/^#: /d" "$i"
-	# Reformat manpage to wrap lines
-	msgcat "$i" > tmp-result.po
-	mv tmp-result.po "$i"
+# Determine directory names from upstream directory.
+directories=$(find ../upstream -maxdepth 1 -type d | cut -d/ -f3- | LC_ALL=C sort)
+
+header=$(mktemp)
+tail=$(mktemp)
+result=$(mktemp)
+
+for directory in $directories; do
+	echo "Processing directory '$directory'"
+	translations=$(find "$directory"/man* -name "*.po" | LC_ALL=C sort)
+	for translation in $translations; do
+		# Get the head of the file until first msgid line
+		# and filter out all comment lines without year information
+		sed -n "1,/^msgid/p" "$translation" |
+		grep -v "^# German translation of manpages" |
+		grep -v "^# This file is distributed under the same license as the manpages-de package." |
+		grep -v "^# Copyright © of this file:" |
+		grep -v "^#\s*$" > "$header"
+		sed -e "1,/^msgid/d" "$translation" > "$tail"
+		echo "# German translation of manpages" > "$result"
+		echo "# This file is distributed under the same license as the manpages-de package." >> "$result"
+		echo "# Copyright © of this file:" >> "$result"
+		cat "$result" "$header" "$tail" > "$translation"
+		# Now remove location information, which is not needed in our case
+		sed -i -e "/^#: /d" "$translation"
+		# Reformat manpage to wrap lines
+		msgcat "$translation" > "$result"
+		mv "$result" "$translation"
+	done
 done
-rm -f tmp-result.po tmp-header.po tmp-tail.po
+rm -f "$header" "$tail" "$result"
