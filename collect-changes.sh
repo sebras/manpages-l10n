44dc186e902f5ce727d7fe020ab1b341ab3e1552
Dr. Tobias Quathamer <toddy@debian.org>
2017-08-04 14:45:54 +0200

    Update change collection

--- collect-changes.sh
+++ collect-changes.sh
@@ -1,47 +1,32 @@
 #!/bin/sh
 set -e
 
-LAST_RELEASE=manpages-de-`git tag -l "manpages-de*" | cut -d- -f3 | sort --version-sort | tail -n1`
+last_release=$(git for-each-ref --count=1 --sort="-committerdate" refs/tags | cut -f2 | cut -d/ -f3)
 
-echo "Version X.X"
+echo "## Version X.X"
 echo
-echo "  "`LC_ALL=C date`
-echo
-echo "  * New translations:"
-git diff --name-status $LAST_RELEASE po/man?/ |
-grep "\.po" |
+echo "* New translations:"
+git diff --name-status $last_release po/*/man*/ |
 grep "^A" |
 cut -f2 |
 sort |
-cut -d"/" -f3 |
+cut -d"/" -f4 |
 sed -e "s/\.po$//" |
 perl -e "my @a; while (<>) { chomp; push(@a, \$_); } print join(', ', @a); print \"\\n\";" |
 fold --spaces --width=68 |
-perl -ne "print '    '; print;"
+perl -ne "print '  '; print;"
 
-echo "  * Updated translations:"
-git diff --name-status $LAST_RELEASE po/man?/ |
-grep "\.po" |
-grep "^M" |
-cut -f2 |
-sort |
-cut -d"/" -f3 |
-sed -e "s/\.po$//" |
-perl -e "my @a; while (<>) { chomp; push(@a, \$_); } print join(', ', @a); print \"\\n\";" |
-fold --spaces --width=68 |
-perl -ne "print '    '; print;"
-
-echo "  * Removed translations:"
-git diff --name-status $LAST_RELEASE po/man?/ |
-grep "\.po" |
+echo "* Removed translations:"
+git diff --name-status $last_release po/man?/ |
 grep "^D" |
 cut -f2 |
 sort |
-cut -d"/" -f3 |
+cut -d"/" -f4 |
 sed -e "s/\.po$//" |
 perl -e "my @a; while (<>) { chomp; push(@a, \$_); } print join(', ', @a); print \"\\n\";" |
 fold --spaces --width=68 |
-perl -ne "print '    '; print;"
+perl -ne "print '  '; print;"
+
 
 echo
 echo
