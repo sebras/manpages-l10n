34b8cd4274bf656fb50fb8c8bb9ca090a8b1870d
Dr. Tobias Quathamer <toddy@debian.org>
2019-01-31 19:08:28 +0100

    Rewrite script for new repository layout

--- po/format-po.sh
+++ po/format-po.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright © 2010-2017 Dr. Tobias Quathamer <toddy@debian.org>
+# Copyright © 2010-2019 Dr. Tobias Quathamer <toddy@debian.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,40 +15,32 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Determine directory names from upstream directory.
-directories=$(find ../upstream -maxdepth 1 -type d | cut -d/ -f3- | LC_ALL=C sort)
-
 header=$(mktemp)
 tail=$(mktemp)
 result=$(mktemp)
 
-for directory in $directories; do
-	echo "Processing directory '$directory'"
-	translations=$(find "$directory"/man* -name "*.po" | LC_ALL=C sort)
-	for translation in $translations; do
-		# Get the head of the file until first msgid line
-		# and filter out all comment lines without year information
-		sed -n "1,/^msgid/p" "$translation" |
-		grep -v "^# German translation of manpages" |
-		grep -v "^# This file is distributed under the same license as the manpages-de package." |
-		grep -v "^# Copyright © of this file:" |
-		grep -v "^#\s*$" > "$header"
-		sed -e "1,/^msgid/d" "$translation" > "$tail"
-		echo "# German translation of manpages" > "$result"
-		echo "# This file is distributed under the same license as the manpages-de package." >> "$result"
-		echo "# Copyright © of this file:" >> "$result"
-		cat "$result" "$header" "$tail" > "$translation"
-		# Now remove location information, which is not needed in our case
-		sed -i -e "/^#: /d" "$translation"
-		# Reformat manpage to wrap lines
-		msgcat "$translation" > "$result"
-		mv "$result" "$translation"
-	done
+translations=$(find man* -name "*.po" | LC_ALL=C sort)
+for translation in $translations; do
+	# Get the head of the file until first msgid line
+	# and filter out all comment lines without year information
+	sed -n "1,/^msgid/p" "$translation" |
+	grep -v "^# German translation of manpages" |
+	grep -v "^# This file is distributed under the same license as the manpages-de package." |
+	grep -v "^# Copyright © of this file:" |
+	grep -v "^#\s*$" > "$header"
+	sed -e "1,/^msgid/d" "$translation" > "$tail"
+	echo "# German translation of manpages" > "$result"
+	echo "# This file is distributed under the same license as the manpages-de package." >> "$result"
+	echo "# Copyright © of this file:" >> "$result"
+	cat "$result" "$header" "$tail" > "$translation"
+	# Reformat manpage to wrap lines
+	msgcat "$translation" > "$result"
+	mv "$result" "$translation"
 done
 
 # Reformat all common messages
 echo "Processing common messages"
-for translation in common-*/*po; do
+for translation in common/*po; do
 	msgcat --force-po "$translation" > "$result"
 	mv "$result" "$translation"
 done
