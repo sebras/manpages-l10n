0e93e0451921f164b93142a2635fe996fbe31f0d
Dr. Tobias Quathamer <toddy@debian.org>
2019-02-07 13:15:11 +0100

    Simplify script for template updates

--- templates/update-all-templates.sh
+++ templates/update-all-templates.sh
@@ -18,69 +18,13 @@
 # Find all translation mandirs
 mandirs=$(find ../po -maxdepth 1 -type d -name "man*"  | cut -d/ -f3- | LC_ALL=C sort)
 
-# Save old templates for comparing the file contents
-mkdir backup
-for i in man*/*; do
-  mv $i backup
-done
-
-# Start with clean directories
-rm -rf man*
-
-tmp1=$(mktemp)
-tmp2=$(mktemp)
-
 for mandir in $mandirs; do
   echo Section $mandir
-  mkdir "$mandir"
 
   # Find all translations of the section
   translations=$(find "../po/$mandir" -type f | cut -d/ -f3- | LC_ALL=C sort)
   for translation in $translations; do
     manpage=$(basename $translation .po)
-
-    # Create pot file
-    echo "Generating pot file for manpage '$manpage'"
-
-    # Get the current time to ensure that all potfiles
-    # have the exact same date and time of creation in the header.
-    datetime=$(date --iso-8601=minutes | sed -e "s/T/ /")
-
-    # Find all upstream manpages with a matching name
-    potfiles=""
-    upstream_manpages=$(find ../upstream/*/ -type f -name "$manpage" | LC_ALL=C sort)
-    for upstream_manpage in $upstream_manpages; do
-      # Create a template for each distribution
-      distribution=$(echo $upstream_manpage | cut -d/ -f3)
-
-      potfile=$(mktemp --tmpdir "$distribution.$manpage.XXX.pot")
-      potfiles="$potfiles $potfile"
-      ./generate-template.sh $distribution $manpage > $potfile
-
-      # Ensure the same timestamp
-      if [ -f "$potfile" ]; then
-        sed -i -e "s/^\"POT-Creation-Date: .*$/\"POT-Creation-Date: $datetime\\\\n\"/" "$potfile"
-      fi
-    done
-
-    # Merge all pot files into one
-    msgcat -o "$mandir/$manpage.pot" $potfiles
-
-    # Remove random characters from multiple file information
-    # and reformat the comment into something more readable, e.g.:
-    # "#. #-#-#-#-#  archlinux.fclose.3.0bX.pot (PACKAGE VERSION)  #-#-#-#-#"
-    sed -i -e "s/^#\. #-#-#-#-# \([^.]*\)\.\(.\+\)\.[A-Za-z0-9]\{3\}\.pot/#. #-#-#-#-# \1: \2.pot/" "$mandir/$manpage.pot"
-
-    # Determine if the only change is the "POT-Creation-Date:" header
-    # If so, copy back the backup to revert that change
-    if [ -f "$mandir/$manpage.pot" -a -f "backup/$manpage.pot" ]; then
-      sed -f remove-potcdate.sed < "backup/$manpage.pot" > "$tmp1"
-      sed -f remove-potcdate.sed < "$mandir/$manpage.pot" > "$tmp2"
-      if cmp "$tmp1" "$tmp2" >/dev/null 2>&1; then
-        mv "backup/$manpage.pot" "$mandir/$manpage.pot"
-      fi
-    fi
+    ./generate-one-template.sh $manpage
   done
 done
-
-rm -rf backup $tmp1 $tmp2 $potfiles
