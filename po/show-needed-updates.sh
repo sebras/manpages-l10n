340b4c4c5e3dbcd50d88bbdfd02e1be9ac6cc825
Dr. Tobias Quathamer <toddy@debian.org>
2019-02-28 17:16:04 +0100

    Update script for statistics

--- po/show-needed-updates.sh
+++ po/show-needed-updates.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright © 2010-2012 Dr. Tobias Quathamer <toddy@debian.org>
+# Copyright © 2010-2019 Dr. Tobias Quathamer <toddy@debian.org>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,47 +15,29 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# Determine directory names from upstream directory.
-directories=$(find ../upstream -maxdepth 1 -type d | cut -d/ -f3- | LC_ALL=C sort)
-
-count=0
-for directory in $directories; do
-	echo "Processing directory '$directory'"
-	translations=$(find "$directory"/man* -name "*.po" | LC_ALL=C sort)
-	for translation in $translations; do
-		stats=$(msgfmt -cv -o /dev/null $translation 2>&1)
-		fuzzy_or_untranslated=$(echo $stats | grep "[0-9]\+[^0-9]\+[0-9]\+")
-		if [ -n "$fuzzy_or_untranslated" ]; then
-			# Remove the last text part
-			all=$(echo $stats | sed -e "s/[^0-9]\+$//")
-			# Replace all remaining text parts with the plus sign
-			all=$(echo $all | sed -e "s/[^0-9]\+/+/g")
-			# Calculate the sum
-			all=$(echo $all | bc)
-			# Get the translated messages
-			translated=$(echo $stats | sed -e "s/\([0-9]\+\).*/\1/")
-			# Calculate the percentage
-			percentage=$(echo "100 * $translated / $all" | bc)
-			# Calculate needed translations for 80%
-			hint=""
-			if [ $percentage -lt 80 ]; then
-				number=$(echo "(800 * $all / 100 + 9) / 10 - $translated" | bc)
-				hint=", es fehlen $number Übersetzungen bis 80%"
-			fi
-			echo $translation: $percentage%$hint
-			echo $stats
-			echo
-			count=$(($count+1))
+translations=$(find man* -name "*.po" | LC_ALL=C sort)
+for translation in $translations; do
+	stats=$(msgfmt -cv -o /dev/null $translation 2>&1)
+	fuzzy_or_untranslated=$(echo $stats | grep "[0-9]\+[^0-9]\+[0-9]\+")
+	if [ -n "$fuzzy_or_untranslated" ]; then
+		# Remove the last text part
+		all=$(echo $stats | sed -e "s/[^0-9]\+$//")
+		# Replace all remaining text parts with the plus sign
+		all=$(echo $all | sed -e "s/[^0-9]\+/+/g")
+		# Calculate the sum
+		all=$(echo $all | bc)
+		# Get the translated messages
+		translated=$(echo $stats | sed -e "s/\([0-9]\+\).*/\1/")
+		# Calculate the percentage
+		percentage=$(echo "100 * $translated / $all" | bc)
+		# Calculate needed translations for 80%
+		hint=""
+		if [ $percentage -lt 80 ]; then
+			number=$(echo "(800 * $all / 100 + 9) / 10 - $translated" | bc)
+			hint=", es fehlen $number Übersetzungen bis 80%"
 		fi
-	done
-done
-
-if [ $count -eq 0 ]; then
-	echo "Alle Dateien sind vollständig übersetzt."
-else
-	if [ $count -eq 1 ]; then
-		echo "1 Datei ist nicht vollständig übersetzt."
-	else
-		echo "$count Dateien sind nicht vollständig übersetzt."
+		echo $translation: $percentage%$hint
+		echo $stats
+		echo
 	fi
-fi
+done
