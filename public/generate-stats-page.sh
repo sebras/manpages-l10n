21f4e5b3abe9dda70bf0fd204c9a47360da33ace
Helge Kreutzmann <debian@helgefjell.de>
2019-11-03 16:11:15 +0100

    Update copyright and DESIGN

--- public/generate-stats-page.sh
+++ public/generate-stats-page.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # Copyright © 2018-2019 Dr. Tobias Quathamer <toddy@debian.org>
+# Copyright © 2019 Helge Kreutzmann <debian@helgefjell.de>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
