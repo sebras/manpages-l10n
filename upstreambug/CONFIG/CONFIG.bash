92d3777e47a23ea29c593b09f8394fc70393dea3
Helge Kreutzmann <debian@helgefjell.de>
2020-05-06 19:26:03 +0200

    reportbugupstream: Add bug reporting details to several CONFIGs

--- upstreambug/CONFIG/CONFIG.bash
+++ upstreambug/CONFIG/CONFIG.bash
@@ -1,4 +1,6 @@
 Homrepage: https://savannah.gnu.org/projects/bash/
+Note: Bug reports for bash should be sent to: bug-bash@gnu.org using the bashbug program -- ignored, since the additional info is not relevant for us
+Bugtype: email bug-bash@gnu.org
 bash.1.po
 clear_console.1.po
 rbash.1.po
