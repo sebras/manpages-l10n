b3def06731f452239245c847785b328fa14bfcd4
Mario Blättermann <mario.blaettermann@gmail.com>
2020-06-25 08:37:43 +0200

    Add link to abandoned Korean man page translation project

--- public/generate-stats-page.sh
+++ public/generate-stats-page.sh
@@ -77,7 +77,7 @@ cat > index.html <<-END_OF_HEADER
        <li><a href="https://web.archive.org/web/20050403211946/http://lme.linux.hu/forditas/index.html">Hungarian (archived website)</a>, appears inactive, last release 2001, license situation unclear</li>
        <li>Indonesian (no web page), last release 1999, appears inactive, license situation unclear</li>
        <li><a href="https://linuxjm.osdn.jp/">Japanese</a>, active, but not current (e.g. partially on the state of 2011/2016)</li>
-       <li>Korean (no web page), appears inactive, last release 2005, license situation unclear</li>
+       <li><a href="https://web.archive.org/web/20060205162241/http://man.kldp.org/wiki">Korean (archived website)</a>, appears inactive, last release 2005, license situation unclear</li>
        <li>Portuguese (no web page), last release 2004</li>
        <li><a href="https://www.transifex.com/kruvalig/man-pages/">Russian</a>, active and current, only man-pages itself (e.g. no coreutils)</li>
        <li><a href="http://es.tldp.org/htmls/pameli.html">Spanish</a>, appears inactive, last known release 2005, based on manpages-1.55</li>
