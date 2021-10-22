3c837e4a5dd6bd4ea91264c8d8129129f65ffc46
Helge Kreutzmann <debian@helgefjell.de>
2021-10-22 14:53:23 +0200

    scripts: Move show-needed-updates.sh to scripts
    Add the localized string to the appropriate config files; however many
    languages are missing this localized string currently

--- po/scripts/show-needed-updates.sh
+++ po/scripts/show-needed-updates.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Copyright © 2010-2019 Dr. Tobias Quathamer <toddy@debian.org>
+#             2021      Dr. Helge Kreutzmann <debian@helgefjell.de>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,6 +15,24 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+if [ a"$1" != a ]; then
+    if [ -d ../$1 ]; then
+        cd ../$1
+    else
+        echo "Language $1 could not be found, aborting"
+        exit 1
+    fi
+    lcode=$1
+else
+    if [ ! -d man1 ]; then
+        echo "No directories with man pages found, aborting"
+        exit 2
+    fi
+    lcode=$(basename $(pwd))
+fi
+
+source ../scripts/l10n_set
 
 translations=$(find man* -name "*.po" | LC_ALL=C sort)
 for translation in $translations; do
@@ -34,7 +53,8 @@ for translation in $translations; do
 		hint=""
 		if [ $percentage -lt 80 ]; then
 			number=$(echo "(800 * $all / 100 + 9) / 10 - $translated" | bc)
-			hint=", на $number преводи недостасуваат до 80%"
+			# $hint1 and $hint2 are localized strings from the configuration file
+			hint="$hint1 $number $hint2"
 		fi
 		echo $translation: $percentage%$hint
 		echo $stats
