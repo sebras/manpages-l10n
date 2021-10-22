3a6158f1ed0fe8e104c152c9dd3077b6508a14dd
Helge Kreutzmann <debian@helgefjell.de>
2021-10-22 17:03:38 +0200

    scripts: Move generate-all-manpages.sh into scripts

--- po/scripts/generate-all-manpages.sh
+++ po/scripts/generate-all-manpages.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
 # Copyright © 2017-2019 Dr. Tobias Quathamer <toddy@debian.org>
+#             2021 Dr. Helge Kreutzmann <debian@helgefjell.de>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,6 +16,22 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+if [ a"$2" != a ]; then
+    if [ -d ../$2 ]; then
+        cd ../$2
+    else
+        echo "Language $2 could not be found, aborting"
+        exit 1
+    fi
+    lcode=$2
+else
+    if [ ! -d man1 ]; then
+        echo "No directories with man pages found, aborting"
+        exit 2
+    fi
+    lcode=$(basename $(pwd))
+fi
+
 # Require distribution name
 if [ -z "$1" ]; then
 	echo "Please specify the distribution." >&2
@@ -26,6 +43,6 @@ echo "Processing distribution '$distribution'"
 manpages=$(find "../../upstream/$distribution"/man* -type f | cut -d/ -f5- | LC_ALL=C sort)
 for manpage in $manpages; do
 	if [ -f "$manpage.po" ]; then
-		./generate-manpage.sh $distribution "$manpage"
+		../scripts/generate-manpage.sh $distribution "$manpage" $lcode
 	fi
 done
