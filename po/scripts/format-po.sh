f322c41afe01fbd2cd9ab102882bd607b2334212
Helge Kreutzmann <debian@helgefjell.de>
2021-10-22 13:42:50 +0200

    scripts: Use language configuration file

--- po/scripts/format-po.sh
+++ po/scripts/format-po.sh
@@ -54,12 +54,12 @@ for translation in $translations; do
 	# Get the head of the file until first msgid line
 	# and filter out all comment lines without year information
 	sed -n "1,/^msgid/p" "$translation" |
-	grep -v "^# $lname translation of manpages" |
+	grep -v "^# $langname translation of manpages" |
 	grep -v "^# This file is distributed under the same license as the manpages-l10n package." |
 	grep -v "^# Copyright © of this file:" |
 	grep -v "^#\s*$" > "$header"
 	sed -e "1,/^msgid/d" "$translation" > "$tail"
-	echo "# $lname translation of manpages" > "$result"
+	echo "# $langname translation of manpages" > "$result"
 	echo "# This file is distributed under the same license as the manpages-l10n package." >> "$result"
 	echo "# Copyright © of this file:" >> "$result"
 	cat "$result" "$header" "$tail" > "$translation"
