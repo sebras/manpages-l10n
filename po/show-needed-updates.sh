176292b382db45a31933c377afc328a0da6c5e41
Mario Blättermann <mario.blaettermann@gmail.com>
2021-11-13 22:06:00 +0100

    Add -h output to show-needed-updates.sh

--- po/show-needed-updates.sh
+++ po/show-needed-updates.sh
@@ -16,6 +16,15 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+if [ "$1" == "-h" ]; then
+  echo "Usage: ./`basename $0` language_code"
+  echo This script shows the translation percentages of *.po files.
+  echo Note, you get only totals instead of distribution-related values.
+  echo ""
+  echo It is mandatory to submit the language code as parameter.
+  exit 0
+fi
+
 if [ -d man1 ]; then
     lcode=$(basename $(pwd))
 elif [ a"$1" != a ]; then
