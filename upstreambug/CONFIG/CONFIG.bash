7d5533d0d6358871dfd0b15e1a7b25a575b63341
Helge Kreutzmann <debian@helgefjell.de>
2022-01-13 16:26:21 +0100

    upstreambug: Add even more man pages to the respective CONFIG

--- upstreambug/CONFIG/CONFIG.bash
+++ upstreambug/CONFIG/CONFIG.bash
@@ -2,6 +2,7 @@ Homrepage: https://savannah.gnu.org/projects/bash/
 Note: Bug reports for bash should be sent to: bug-bash@gnu.org using the bashbug program -- ignored, since the additional info is not relevant for us
 Bugtype: email bug-bash@gnu.org
 bash.1.po
+builtins.1.po
 clear_console.1.po
 rbash.1.po
 bashbug.1.po
--- upstreambug/CONFIG/CONFIG.perl
+++ upstreambug/CONFIG/CONFIG.perl
@@ -2,4 +2,5 @@ Homepage: http://dev.perl.org/perl5/
 Note: Please report bugs to us at the GitHub issue tracker at https://github.com/Perl/perl5/issues
 Bugtype: web https://github.com/Perl/perl5/issues
 perl.1.po
+perl.1perl.po
 perlbook.1.po
