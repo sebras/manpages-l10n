422d3b9a60c9cf8c6f1e24987a431a7692903337
Mario Blättermann <mario.blaettermann@gmail.com>
2022-02-24 15:12:54 +0100

    [de] Avoid empty lines in addendum creation

--- po/de/generate-addendum.sh
+++ po/de/generate-addendum.sh
@@ -39,14 +39,15 @@ number_translators=$(echo "$translators" | wc -l)
 
 # Output of common header
 echo "PO4A-HEADER:mode=after;position=^\.(TH|Dt);beginboundary=FakePo4aBoundary" > "$addendum"
-echo >> "$addendum"
 
 # Special case for manpages which use mdoc syntax
 if [ $ismdoc ]; then
     # MDOC File
+    echo ".Pp" >> "$addendum"
     echo ".Sh ÜBERSETZUNG" >> "$addendum"
 else
     # Groff file
+    echo ".PP" >> "$addendum"
     echo ".SH ÜBERSETZUNG" >> "$addendum"
 fi
 echo "Die deutsche Übersetzung dieser Handbuchseite wurde von" >> "$addendum"
